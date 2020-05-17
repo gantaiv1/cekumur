489ba87a41b3989c2af0c858d47d31e5b81abe85
gantaiv1 <65419003+gantaiv1@users.noreply.github.com>
2020-05-17 18:45:47 +0700

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,8 +1,8 @@
 print("mari kita lihat umur anda dalam menit,detik, dan hari")
 nama = input("nama:" )
 print("sekarang masukan umur anda")
-age = int(input("age:" ) )
-umur = age * 365
-menit = age * 525948
-detik = age * 525948
+umur = int(input("umur:" ))
+umur = umur * 365
+menit = umur * 525948
+detik = umur * 525948
 print(name, "telah hidup selama", hari,"hari", menit, "menit, dan", detik, "detik! Wah!")
